bbaed2e6e5db17898a65dc2e614d27137d010003
George T Kramer <george.t.kramer@intel.com>
2017-03-03 19:17:56 -0800

    Fix DNS conflict between systemd and dnsmasq
    Using the option DNSStubListener=no to disable systemd's DNS stub listener.  Also adding support for IPv6 NAT and handling hostnames consistently for all web-based services.
    
    Do not run install scripts if requirements are not met
    
    Using more portable shell conditional expressions
    
    Naming install scripts for a clear entry point
    
    Removing unnecessary firewall dependencies
    
    Properly sequencing startup of servers
    
    Properly limiting servers on interfaces
    
    Defaulting to system-preferred DNS resolver
    
    Systems often have their preferred DNS resolvers (e.g. systemd-resolved, NetworkManager).  Configuration is simplified by accomodating this fact and not using the DNS stub provided by dnsmasq.
    
    Passing server domain to PXE configuration
    
    Bug fixes for enabling automated install

--- app/settings.py
+++ app/settings.py
@@ -1,7 +1,4 @@
 import os
-
-# __file__ refers to the file settings.py 
-# APP_ROOT = application_top
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   
 APP_STATIC = os.path.join(APP_ROOT, 'static')
-SUBDIR = '/icis'
+SUBDIR = os.path.join('/', os.path.basename(APP_ROOT))
